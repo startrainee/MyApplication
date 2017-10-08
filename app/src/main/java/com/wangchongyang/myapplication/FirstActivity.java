30b3cb281a5ce71701657d2df6525a01dbc61e6a
Wcy <1334088767@qq.com>
2017-10-08 18:14:42 +0800

    change to FirstActivity.

--- app/src/main/java/com/wangchongyang/myapplication/FirstActivity.java
+++ app/src/main/java/com/wangchongyang/myapplication/FirstActivity.java
@@ -1,13 +1,23 @@
 package com.wangchongyang.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class FirstActivity extends Activity {
+public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
     }
+
+    public void startSecondActivity(View view) {
+            Intent intent = new Intent();
+            intent.setClass(this,SecondActivity.class);
+            startActivity(intent);
+        }
 }
